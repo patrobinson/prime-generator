51c45ae52419d018a81d8b9cdf4f4131eefb2e96
Patrick Robinson <patrick.robinson@envato.com>
2022-03-14 18:48:28 +1100

    Copy code from miller rabin
    Webasm doesn't support threads so we must modify this code to use iter() instead of par_iter()

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@ use sha2::{Digest, Sha256};
 use rand::prelude::*;
 use rand_chacha::ChaCha12Rng;
 use hex::FromHex;
+use num_bigint::{ToBigUint, RandBigInt, BigUint};
+use num_traits::{One, Zero};
+use std::iter::repeat_with;
 
 mod utils;
 
@@ -43,6 +46,7 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
     router
         .get("/", |_, _| Response::ok("Hello from Workers!"))
+        // Generates a 2048bit random number
         .get_async("/random", |_, _ctx| async move {
             let rand = random().await;
             if let Err(e) = rand {
@@ -51,6 +55,16 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
 
             Response::ok(rand.unwrap().to_string())
         })
+        .get_async("/prime", |_, _ctx| async move {
+            let rand = random().await;
+            if let Err(e) = rand {
+                return Response::error(e.to_string(), 500)
+            }
+
+            let p = find_next_prime(rand.unwrap());
+
+            Response::ok(p.to_string())
+        })
         .get("/worker-version", |_, ctx| {
             let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
             Response::ok(version)
@@ -58,7 +72,7 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
         .run(req, env).await
 }
 
-async fn random() -> std::result::Result<u64, std::string::String> {
+async fn random() -> std::result::Result<num_bigint::BigUint, std::string::String> {
     let mut hasher = Sha256::new();
     let response = reqwest::get("https://drand.cloudflare.com/public/latest").await;
     if let Err(e) = response {
@@ -74,5 +88,119 @@ async fn random() -> std::result::Result<u64, std::string::String> {
     let (seed, _) = buf.split_array_ref::<32>();
 
     let mut rng = ChaCha12Rng::from_seed(*seed);
-    Ok(rng.gen_range(0..100000000) as u64)
+    Ok(rng.gen_biguint(2048))
+}
+
+fn find_next_prime(n: num_bigint::BigUint) -> num_bigint::BigUint {
+    let mut n = match is_odd(&n) {
+        true => n,
+        false => n + 1 as u8
+    };
+    while !is_prime(&n, 1) {
+        n = n + num_bigint::BigUint::from(2 as u8)
+    };
+    n
+}
+
+fn is_odd(n: &num_bigint::BigUint) -> bool {
+    !(n % num_bigint::BigUint::from(2 as u8) == num_bigint::BigUint::from(0 as u8))
+}
+
+// This is copied from https://github.com/cjayross/miller_rabin
+
+/*
+MIT License
+
+Copyright (c) 2020 Calvin Jay Ross
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+macro_rules! biguint {
+    ($e:expr) => {
+        ($e).to_biguint().unwrap()
+    };
+}
+
+fn decompose(n: &BigUint) -> (BigUint, BigUint) {
+    let one = One::one();
+    let ref two = biguint!(2);
+    let mut d: BigUint = (n - 1u8).clone();
+    let mut r: BigUint = Zero::zero();
+
+    while &d % two == one {
+        d /= two;
+        r += 1u8;
+    }
+
+    (d, r)
+}
+
+fn miller_rabin(a: &BigUint, n: &BigUint, d: &BigUint, r: &BigUint) -> bool {
+    let n_minus_one: BigUint = n - 1u8;
+    let mut x = a.modpow(d, n);
+    let mut count: BigUint = One::one();
+    let ref two = biguint!(2);
+
+    if x == One::one() || x == n_minus_one {
+        return false;
+    }
+
+    while &count < r {
+        x = x.modpow(two, n);
+
+        if x == n_minus_one {
+            return false;
+        }
+
+        count += 1u8;
+    }
+
+    true
+}
+
+pub fn is_prime<T: ToBigUint>(n: &T, k: usize) -> bool {
+    let ref n = biguint!(n);
+    let n_minus_one: BigUint = n - 1u8;
+    let (ref d, ref r) = decompose(n);
+
+    if n <= &One::one() {
+        return false;
+    } else if n <= &biguint!(3) {
+        return true;
+    } else if n <= &biguint!(0xFFFF_FFFF_FFFF_FFFFu64) {
+        let samples: Vec<u8> = vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37];
+        return samples
+            .iter()
+            .filter(|&&m| biguint!(m) < n_minus_one)
+            .find(|&&a| miller_rabin(&biguint!(a), n, d, r))
+            .is_none();
+    }
+
+    let mut rng = rand::thread_rng();
+    let samples: Vec<BigUint> = repeat_with(|| rng.gen_biguint(n_minus_one.bits()))
+        .filter(|m| m < &n_minus_one)
+        .take(k)
+        .collect();
+
+    samples
+        .iter()
+        .find(|&a| miller_rabin(a, n, d, r))
+        .is_none()
 }
